690f45463f6666e8e39f069fec86bb1ca903d684
Ben <benjamintdube@gmail.com>
2020-02-18 18:09:46 -0500

    Finally made websocket work

--- websocket_example_code.py
+++ websocket_example_code.py
@@ -1,5 +1,7 @@
 url= 'https://github.com/websocket-client/websocket-client'
 
+import requests
+import json
 from jarvis import Jarvis
 
 J=Jarvis()
@@ -15,8 +17,22 @@ def load_token(filename):
         return f.read()
 
 def on_message(ws, message):
-   # print(J.onMessage(message))
-    print(message)
+    message=json.loads(message)
+    if message:
+        if message.get('type',None)=='message': 
+           print(message)
+           reply_text=J.onMessage(message)
+           if reply_text:
+               reply=json.dumps({
+                       "id": 1,
+                       "type": "message",
+                       "channel": message['channel'],
+                       "text": reply_text})
+               ws.send(reply)
+    #except:
+   #     print (message)
+    
+       
 
 def on_error(ws, error):
     print(error)
@@ -26,31 +42,28 @@ def on_close(ws):
 
 def on_open(ws):
     def run(*args):
- #       for i in range(3):
-  #          time.sleep(1)
-   #         ws.send("Hello %d" % i)
-    #    time.sleep(1)
-     #   ws.close()
-        print("thread terminating...")
+       pass
+        # for i in range(3):
+          #  time.sleep(1)
+         #   ws.send("Hello %d" % i)
+        #time.sleep(1)
+        #ws.close()
+        #print("thread terminating...")
     thread.start_new_thread(run, ())
 
 
 if __name__ == "__main__":
-    token=load_token('token.txt')
-    websocket.enableTrace(True)
- #   ws = websocket.create_connection("ws://https://slack.com/api/auth.test",  
-   #                                  header=[f'token: {token}']
-    #                                 )
+    token=load_token('token2.txt')
     
-    ws = websocket.WebSocket()
-    ws.connect(', 
-                                
-                              )
-                                                 
- #                             on_open= on_open,
-   #                           on_message = on_message,
-    #                          on_error = on_error,
-     #                         on_close = on_close)
-   # ws.on_open = on_open
+    websocket.enableTrace(True)
+    response=requests.get(f'https://slack.com/api/rtm.connect?token={token}&pretty=1').json()
+    print(response)
+    url=response['url'].replace('\/', '/')
+    ws = websocket.WebSocketApp(url=url,
+                             on_open= on_open,
+                             on_message = on_message,
+                             on_error = on_error,
+                             on_close = on_close)
+    ws.on_open = on_open
     ws.run_forever()
 
